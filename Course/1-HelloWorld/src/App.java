59110df4208359945c1fdfe49a2b9c401d67c0f6
MaloDaHood <malomr2002@gmail.com>
2022-06-21 23:05:50 +0200

    First section done

---
+++ Course/1-HelloWorld/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.print("Second statement on different line");
+
+        // ...println("string") == ...print("string\n")
+
+    }
+}
