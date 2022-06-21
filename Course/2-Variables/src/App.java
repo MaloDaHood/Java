c9bda4f5d1d5b8d92d4f9688e16c52b09c5f1e2d
MaloDaHood <malomr2002@gmail.com>
2022-06-21 23:28:14 +0200

    Section 2 Variables done

---
+++ Course/2-Variables/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        int x; // declaration
+
+        x = 123; // assignment
+
+        int integer = 321; // initialization
+
+        double decimal = 3.14;
+
+        boolean state = true;
+
+        char letter = 'A';
+
+        String text = "Hello World";
+
+    }
+}
