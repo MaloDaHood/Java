dfb78dbeba597dccfc1e4c9d1b7c77130a7746c3
MaloDaHood <malomr2002@gmail.com>
2022-06-22 00:04:15 +0200

    Section 5 done

---
+++ Course/5-Expressions/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        // expression = operands & operators
+        // operands = values or variables
+        // operators = + - * / %
+
+        int friends = 10;
+
+        friends = friends + 1;
+        friends++;
+
+        friends = friends * 2;
+        friends *= 2;
+        
+    }
+}
