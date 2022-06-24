48c136ab84cf9a0cc32d3cc406812138e5da9554
MaloDaHood <malomr2002@gmail.com>
2022-06-24 17:15:46 +0200

    Conditions done

---
+++ Course/9-Conditions/src/App.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How old are you ?");
+
+        int age = scanner.nextInt();
+
+        if(age >= 75) {
+            System.out.println("OK boomer.");
+        }
+        else if(age >= 18) {
+            System.out.println("You are an adult.");
+        }
+        else if(age >= 13) {
+            System.out.println("You are a teenager.");
+        }
+        else {
+            System.out.println("You are not an adult.");
+        }
+
+        scanner.close();
+    }
+}
